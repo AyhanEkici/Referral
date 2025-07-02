374063ceb7575f34c7eb211eff40bb02982776e8
Ayhan Ekici <47488631+AyhanEkici@users.noreply.github.com>
2025-07-02 01:56:08 +0000

    Fix syntax in Http Kernel

---
+++ app/Http/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    // You can leave this empty or use the default Laravel 8 content.
+}
